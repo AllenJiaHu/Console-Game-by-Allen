0f94bc205dfe5b13d1c6ca010cd3a26ac6576b9a
BuildTools <unconfigured@null.spigotmc.org>
2016-10-21 17:52:50 -0400

    firstentry

---
+++ scripts.js
@@ -0,0 +1,166 @@
+$(document).ready(function(){
+
+//FIRST ROW
+$(".button").click(function() {        
+
+var answer = prompt('あ');
+
+	if (answer == 'a') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> あ a </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> あ a </h1>")
+	}
+
+	});
+
+
+
+$(".button1").click(function() {        
+
+var answer = prompt('い');
+
+	if (answer == 'i') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> い i </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> い i </h1>")
+	}
+
+	});
+
+
+
+$(".button2").click(function() {        
+
+var answer = prompt('う');
+
+	if (answer == 'u') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> う u </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> う u </h1>")
+	}
+
+	});
+
+
+
+$(".button3").click(function() {        
+
+var answer = prompt('え');
+
+	if (answer == 'e') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> え e </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> え e </h1>")
+	}
+
+	});
+
+
+
+$(".button4").click(function() {        
+
+var answer = prompt('お');
+
+	if (answer == 'o') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> お o </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> お o </h1>")
+	}
+
+	});
+
+// SECOND ROW
+
+$(".button5").click(function() {        
+
+var answer = prompt('か');
+
+	if (answer == 'ka') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> か ka </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> か ka </h1>")
+	}
+
+	});
+
+
+
+$(".button6").click(function() {        
+
+var answer = prompt('き');
+
+	if (answer == 'ki') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> き ki </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> き ki </h1>")
+	}
+
+	});
+
+
+
+$(".button7").click(function() {        
+
+var answer = prompt('く');
+
+	if (answer == 'ku') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> く ku </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> く ku </h1>")
+	}
+
+	});
+
+
+
+$(".button8").click(function() {        
+
+var answer = prompt('け');
+
+	if (answer == 'ke') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> け ke </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> け ke </h1>")
+	}
+
+	});
+
+
+
+
+$(".button9").click(function() {        
+
+var answer = prompt('こ');
+
+	if (answer == 'ko') {
+		$("body").css("background-color", "#66ff33")
+		$("body").html("<h1 class='text'> こ ko </h1>")
+	} else {
+		$("body").css("background-color", "#ff0000")
+		$("body").html("<h1 class='text'> こ ko </h1>")
+	}
+
+	});
+
+
+
+});
+
